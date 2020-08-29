a03bf4dd3dd197dcc86db5d41a35a0cf0cbc1bbc
Taeyoon Kim <tykimseoul@gmail.com>
2020-08-30 00:20:48 +0900

    update: visualize data crunched

--- venv/preprocess.py
+++ venv/preprocess.py
@@ -6,7 +6,7 @@ import re
 import datetime
 
 pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 1000)
+pd.set_option('display.width', 2000)
 pd.set_option('display.max_colwidth', 200)
 pd.set_option('display.max_rows', None)
 
@@ -57,22 +57,33 @@ def remove_header(sent):
         raise
 
 
+dfo = pd.read_csv('./output_20200826.csv')
+dfo.dropna(inplace=True)
+dfo['len'] = dfo.apply(lambda r: len(str(r['content'])), axis=1)
+dfo.sort_values('len', inplace=True, ascending=True)
+dfo.drop(dfo[dfo['len'] > 6000].index, inplace=True)
+dfo.reset_index(inplace=True, drop=True)
+dfo['cumsum'] = dfo['len'].cumsum()
+print(dfo.head(100))
+
 kkma = Kkma()
 df = pd.read_csv('./output_20200826.csv')
 df.dropna(inplace=True)
-df['len'] = df.apply(lambda r: len(str(r['content'])), axis=1)
 df['sents'] = df.apply(lambda r: sent_tokenize(r['content']), axis=1)
 df['sents_stripped'] = df.apply(lambda r: strip_sent(r['sents']), axis=1)
 df.dropna(inplace=True)
+df['len'] = df['sents_stripped'].str.join(' ').str.len()
 df['sent_count'] = df.apply(lambda r: len(r['sents_stripped']), axis=1)
-df.sort_values('sent_count', inplace=True, ascending=True)
+df.sort_values('len', inplace=True, ascending=True)
 df.reset_index(inplace=True, drop=True)
 df.drop(df[df['sent_count'] < 2].index, inplace=True)
 df.drop(df[df['len'] > 6000].index, inplace=True)
 print(df.head(200))
 df['cumsum'] = df['len'].cumsum()
-df['prp'] = df['cumsum'] / max(df['cumsum'])
-# df['cumsum'].plot()
-df['sent_count'].hist(bins=100)
-print(df.head())
+
+dfo['cumsum'].plot()
+df['cumsum'].plot()
+plt.show()
+dfo['len'].hist(bins=100)
+df['len'].hist(bins=100, alpha=0.7)
 plt.show()
