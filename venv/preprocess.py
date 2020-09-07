9bd5aeffdd55118b092418f36a1d695aa95e040c
Taeyoon Kim <tykimseoul@gmail.com>
2020-09-07 17:56:47 +0900

    update: preprocess document morphemes

--- venv/preprocess.py
+++ venv/preprocess.py
@@ -2,20 +2,27 @@
 # -*- coding: <utf-8> -*-
 import pandas as pd
 import matplotlib.pyplot as plt
-from konlpy.tag import Kkma
+from konlpy.tag import Kkma, Okt, Komoran, Hannanum
+import kss
 import nltk
 import re
 import datetime
+from collections import Counter
+import os
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 2000)
 pd.set_option('display.max_colwidth', 200)
 pd.set_option('display.max_rows', None)
 
+josa = '(' + ')|('.join(['JK[A-Z]', 'JX', 'JC']) + ')'
+eomi = '(' + ')|('.join(['E[A-Z]+', 'X[A-Z]+']) + ')'
+symbol = '(' + ')|('.join(['S[A-Z]']) + ')'
+
 
 def sent_tokenize(text):
     text = re.sub(r'\s+', ' ', text)
-    sents = nltk.sent_tokenize(text)
+    sents = kss.split_sentences(text)
     return sents
 
 
@@ -46,7 +53,7 @@ def strip_sent(sents):
             pass
     word_exclusions = "(" + ")|(".join([r'.+@.+\..+', r'.*ⓒ.*', r'.*Copyright.*', r'.*▶.*']) + ")"
     result = list(filter(lambda s: not re.match(word_exclusions, s), result))
-    stop_phrases = ['공감언론 뉴시스가 독자 여러분의 소중한 제보를 기다립니다.', '드링킷!', '난 뉴스가 찾아와!', '※ \'당신의 제보가 뉴스가 됩니다\' YTN은 여러분의 소중한 제보를 기다립니다.', 'MBC 뉴스는 24시간 여러분의 제보를 기다립니다.']
+    stop_phrases = ['공감언론 뉴시스가 독자 여러분의 소중한 제보를 기다립니다.', '드링킷', '※ \'당신의 제보가 뉴스가 됩니다\' YTN은 여러분의 소중한 제보를 기다립니다.']
     result = list(filter(lambda s: s not in stop_phrases, result))
     if len(result) == 0:
         return None
@@ -66,41 +73,40 @@ def remove_header(sent):
         raise
 
 
-file_path = './news_data/20200828.csv'
-dfo = pd.read_csv(file_path)
-dfo.dropna(inplace=True)
-dfo['len'] = dfo.apply(lambda r: len(str(r['content'])), axis=1)
-dfo.sort_values('len', inplace=True, ascending=True)
-dfo.drop(dfo[dfo['len'] > 6000].index, inplace=True)
-dfo.reset_index(inplace=True, drop=True)
-dfo['cumsum'] = dfo['len'].cumsum()
-print(dfo.head(100))
+def extract_morphemes(komoran, sent):
+    try:
+        morphs = komoran.pos(sent)
+    except UnicodeDecodeError:
+        return None
+    morphs = list(filter(lambda m: not re.match(josa, m[1]) and not re.match(eomi, m[1]) and not re.match(symbol, m[1]), morphs))
+    print(morphs)
+    return morphs
 
-# kkma = Kkma()
-df = pd.read_csv(file_path)
-df.dropna(inplace=True)
-df.drop_duplicates('content', keep='first', inplace=True)
-df['sents'] = df.apply(lambda r: sent_tokenize(r['content']), axis=1)
-df['sents_stripped'] = df.apply(lambda r: strip_sent(r['sents']), axis=1)
-df.dropna(inplace=True)
-df['len'] = df['sents_stripped'].str.join(' ').str.len()
-df.drop(df[df['len'] > 6000].index, inplace=True)
-df.drop(df[df['len'] < 200].index, inplace=True)
-df['sent_count'] = df.apply(lambda r: len(r['sents_stripped']), axis=1)
-df.sort_values('len', inplace=True, ascending=True)
-df.drop(df[df['sent_count'] < 3].index, inplace=True)
-df.reset_index(inplace=True, drop=True)
-print(df.head(200))
-df['cumsum'] = df['len'].cumsum()
-# flat = [item for sublist in df['sents_stripped'].tolist() for item in sublist]
-# dff = pd.DataFrame(flat)
-# dff.columns = ['sents']
-# print(dff['sents'].value_counts().nlargest(n=20))
-# print(len(flat))
 
-dfo['cumsum'].plot()
-df['cumsum'].plot()
-plt.show()
-dfo['len'].hist(bins=100)
-df['len'].hist(bins=100, alpha=0.7)
-plt.show()
+file_path = './news_data/'
+
+komoran = Komoran()
+for news in sorted(os.listdir(file_path)):
+    news_file = file_path + news
+    df = pd.read_csv(news_file, engine='python')
+    df.dropna(inplace=True)
+    df.drop_duplicates('content', keep='first', inplace=True)
+    df['sents'] = df.apply(lambda r: sent_tokenize(r['content']), axis=1)
+    df['sents_stripped'] = df.apply(lambda r: strip_sent(r['sents']), axis=1)
+    df.dropna(inplace=True)
+    df['content_stripped'] = df['sents_stripped'].str.join(' ')
+    df['len'] = df['content_stripped'].str.len()
+    df.drop(df[df['len'] > 2000].index, inplace=True)
+    df.drop(df[df['len'] < 200].index, inplace=True)
+    df.sort_values('len', inplace=True, ascending=False)
+    df['morphemes'] = df.apply(lambda r: extract_morphemes(komoran, r['content_stripped']), axis=1)
+    df.dropna(inplace=True)
+    # df['morph_size'] = df.apply(lambda r: len(r['morphemes']), axis=1)
+    # df['freqs'] = df.apply(lambda r: Counter(r['morphemes']).most_common(10), axis=1)
+    # df['sent_count'] = df.apply(lambda r: len(r['sents_stripped']), axis=1)
+    # df.drop(df[df['sent_count'] < 3].index, inplace=True)
+    df.reset_index(inplace=True, drop=True)
+    print(df.head(100))
+    # df['cumsum'] = df['len'].cumsum()
+
+    df[['title', 'date', 'category', 'morphemes']].to_csv('./processed/' + news)
