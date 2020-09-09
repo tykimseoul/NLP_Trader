aa98c61cac2fe99a902f24708cbe3ac7c84655e4
Taeyoon Kim <tykimseoul@gmail.com>
2020-09-09 17:28:17 +0900

    update: now prepare data for a single stock at a time

--- venv/prepare.py
+++ venv/prepare.py
@@ -8,14 +8,9 @@ import os
 import numpy as np
 from datetime import datetime, date, timedelta
 
-vocab_size = 50000
-tokenizer = Tokenizer(num_words=vocab_size)
-morpheme_size = 500
 
-stock_path = './stock_data/'
-stocks = []
-for code in os.listdir(stock_path):
-    stock_file = stock_path + code
+def encode_stock_data(code):
+    stock_file = './stock_data/stock_data_{}.csv'.format(code)
     df = pd.read_csv(stock_file, index_col=0)
     df.reset_index(drop=True, inplace=True)
     df['종가'] = df['종가'].astype(float)
@@ -24,70 +19,78 @@ for code in os.listdir(stock_path):
     df.dropna(inplace=True)
     df['변화율'] = df['변화율'].astype(int)
     df['encoding'] = df.apply(lambda r: np.array(to_categorical(r['변화율'], num_classes=3)), axis=1)
-    print(df.head(10))
-    if len(stocks) == 0:
-        stocks.append(df['날짜'])
-    stocks.append(df[['변화율']])
+    print(df.head())
+    return df[['날짜', 'encoding']]
 
-full_df = pd.concat(stocks, axis=1)
-print(full_df.head())
+
+full_df = encode_stock_data('017670')
 dates = full_df['날짜'].tolist()
 dates.append(None)
 date_pairs = list(zip(dates[1:], dates[:-1]))
-print('pairs', date_pairs)
 
 news_path = './processed/'
-news_days = len(os.listdir(news_path))
-y_train = []
-x_train = []
 
-morphemes = []
-for file in sorted(os.listdir(news_path), reverse=True):
-    news_file = news_path + file
+vocab_size = 50000
+tokenizer = Tokenizer(num_words=vocab_size)
+morpheme_size = 500
+morpheme_data = {}
 
-    df = pd.read_csv(news_file)
-    df['morphemes'] = df.apply(lambda r: list(ast.literal_eval(r['morphemes'])), axis=1)
-    df['morphemes'] = df.apply(lambda r: list(map(str, r['morphemes']))[:morpheme_size], axis=1)
-    x_data = df['morphemes'].tolist()
-    morphemes.extend(x_data)
 
-tokenizer.fit_on_texts(morphemes)
-print(tokenizer.word_counts)
+def fit_tokenizer(tokenizer):
+    morphemes = []
+    for file in sorted(os.listdir(news_path), reverse=True):
+        news_file = news_path + file
 
-for file in sorted(os.listdir(news_path), reverse=True):
-    print(file)
-    news_date = datetime.strptime(file, '%Y%m%d.csv')
-    stock_date = None
-    for pair in date_pairs:
-        first = datetime.strptime(pair[0], '%Y.%m.%d')
-        second = datetime.strptime(pair[1], '%Y.%m.%d')
-        if first <= news_date < second:
-            stock_date = second
-            break
+        df = pd.read_csv(news_file)
+        df = df.sample(min(5000, len(df)))
+        df['morphemes'] = df.apply(lambda r: list(ast.literal_eval(r['morphemes'])), axis=1)
+        df['morphemes'] = df.apply(lambda r: list(map(str, r['morphemes']))[:morpheme_size], axis=1)
+        x_data = df['morphemes'].tolist()
+        morpheme_data[file] = x_data
+        morphemes.extend(x_data)
 
-    news_file = news_path + file
+    tokenizer.fit_on_texts(morphemes)
+    print(tokenizer.word_counts)
+    return tokenizer
 
-    df = pd.read_csv(news_file)
-    df['morphemes'] = df.apply(lambda r: list(ast.literal_eval(r['morphemes'])), axis=1)
-    df['morphemes'] = df.apply(lambda r: list(map(str, r['morphemes']))[:morpheme_size], axis=1)
 
-    x_data = df['morphemes'].tolist()
-    x_data = tokenizer.texts_to_sequences(x_data)
-    x_data = pad_sequences(x_data, maxlen=morpheme_size)
-    print(x_data.shape)
-    x_train.append(x_data)
+tokenizer = fit_tokenizer(tokenizer)
 
-    stock_data = full_df[full_df['날짜'] == stock_date.strftime('%Y.%m.%d')]
-    stock_data = stock_data.drop(['날짜'], axis=1)
-    stock_data = np.array(stock_data.values.tolist())
-    stock_data = np.concatenate(stock_data, axis=None)
-    print(stock_data)
-    y_train.append(np.tile(stock_data, (len(df), 1)))
 
-x_train = np.concatenate(x_train)
-print(x_train.shape)
-y_train = np.concatenate(y_train)
-print(y_train.shape)
+def prepare_data():
+    y_train = []
+    x_train = []
+    for file in sorted(os.listdir(news_path), reverse=True):
+        print(file)
+        news_date = datetime.strptime(file, '%Y%m%d.csv')
+        stock_date = None
+        for pair in date_pairs:
+            first = datetime.strptime(pair[0], '%Y.%m.%d')
+            second = datetime.strptime(pair[1], '%Y.%m.%d')
+            if first <= news_date < second:
+                stock_date = second
+                break
 
-np.save('x_train.npy', x_train)
-np.save('y_train.npy', y_train)
+        x_data = morpheme_data[file]
+        x_data = tokenizer.texts_to_sequences(x_data)
+        x_data = pad_sequences(x_data, maxlen=morpheme_size)
+        print(x_data.shape)
+        x_train.append(x_data)
+
+        stock_data = full_df[full_df['날짜'] == stock_date.strftime('%Y.%m.%d')]
+        stock_data = stock_data.drop(['날짜'], axis=1)
+        stock_data = np.array(stock_data.values.tolist())
+        stock_data = np.concatenate(stock_data, axis=None)
+        print(stock_data)
+        y_train.append(np.tile(stock_data, (len(x_data), 1)))
+
+    x_train = np.concatenate(x_train)
+    print(x_train.shape)
+    y_train = np.concatenate(y_train)
+    print(y_train.shape)
+
+    np.save('x_train.npy', x_train)
+    np.save('y_train.npy', y_train)
+
+
+prepare_data()
